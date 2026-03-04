1f22f184d6edaa4bd9c0a274f0a70693cc84641a
Hamza <hamzaakmal98@gmail.com>
2026-03-03 21:32:27 -0500

    chore: ml remnant cleanup, adding readme

--- cadream/backend/print_boundary_total_pixels_input_sample_1.py
+++
@@ -1,7 +0,0 @@
-import glob
-import numpy as np
-
-# Print the sum of the boundary channel for all validation crops of Input_Sample_1
-for npy_path in glob.glob('../../dataset/Input_Sample_1/crops/*/input.npy'):
-    arr = np.load(npy_path)
-    print(npy_path, 'boundary_total_pixels:', int(arr[4].sum()))
